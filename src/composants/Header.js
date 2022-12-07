2f871be113713ce13b6b3043a3fffb332b2b3818
Bastien-Gaillard <gaillardbastienjf@gmail.com>
2022-12-07 13:48:32 +0100

    Routage + mui

---
+++ src/composants/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet, Link } from "react-router-dom";
+function Header() {
+
+	return (
+		<div>
+            <h1>Ainori</h1>
+		</div>
+	)
+}
+
+export default Header
