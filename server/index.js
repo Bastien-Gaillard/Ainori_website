5fa8b59e0de1e32eb9ce0c3b920b9ebe063fd937
Bastien-Gaillard <gaillardbastienjf@gmail.com>
2022-12-07 09:15:08 +0100

    Base du projet

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const { PrismaClient } = require('@prisma/client');
+
+const express = require('express');
+const path = require('path'); // NEW
+const prisma = new PrismaClient()
+
+const app = express();
+const port = process.env.PORT || 3000;
+const DIST_DIR = path.join(__dirname, '../dist'); // NEW
+const HTML_FILE = path.join(DIST_DIR, 'index.html'); // NEW
+const mockResponse = {
+  foo: 'bar',
+  bar: 'foo'
+};
+app.use(express.static(DIST_DIR)); // NEW
+app.get('/api', (req, res) => {
+  res.send(mockResponse);
+});
+app.get('/', (req, res) => {
+  res.sendFile(HTML_FILE); // EDIT
+});
+
+app.listen(port, function () {
+  console.log('App listening on port: ' + port);
+});
+
+app.get('/api/get/users', async (req, res) => {
+  const users = await prisma.users.findMany()
+  res.send(users);
+});
+
+
---
+++ src/composants/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+
+export default function App() {
+
+  
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+}
