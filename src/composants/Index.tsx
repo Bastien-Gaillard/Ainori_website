3e4eb9aad3ab070da980899464dd2b0679150af1
Bastien-Gaillard <83158666+Bastien-Gaillard@users.noreply.github.com>
2023-06-01 19:43:34 +0200

    fix import

--- src/composants/Index.tsx
+++ src/composants/Index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Header from './Header/Header';
+import Header from './header/Header';
 const instance = axios.create({
     baseURL: 'http://localhost:3001/',
 });
