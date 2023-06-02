bb93eac6fdedce6f76a8469fe1f61559452dfe50
thomas <thomas.barron@formation-technologique.fr>
2023-06-02 13:48:35 +0200

    error

--- src/composants/header/Header.tsx
+++ src/composants/header/Header.tsx
@@ -92,7 +92,7 @@ export default function Header({ socket, updateImage, role }) {
 						textDecoration: 'none',
 					}} href='/allRoutes'>
 						<img style={{ width: '34px', height: '34px' }} src='logo.png' alt="" />
-						<h1 style={{color:"red" ,fontFamily: 'Calibri'}}>Ainori Admin</h1>
+						<h1 style={{fontFamily: 'Calibri'}}>Ainori <span style={{color:"red"}}> Admin </span></h1>
 					</Link>
 					<Nav value={navValue} role={role} />
 					<ProfilNav onNavChange={handleNavChange} socket={socket} updateImage={updateImage} role={role} />
